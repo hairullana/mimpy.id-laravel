6c49b6b31b420787e17b8b3b721a1f49011bfc74
Hairul Lana <hairullana99@gmail.com>
2021-12-21 11:48:55 +0800

    add profilecontroller, change number to phone

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index()
+    {
+        return view('profile.index');
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
