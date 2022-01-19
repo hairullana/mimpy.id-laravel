030aa7490c2fdee87f3101d440e396152d992d2d
Hairul Lana <hairullana99@gmail.com>
2022-01-19 10:02:51 +0800

    fix navbar landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,103 +2,79 @@
 <html class="no-js" lang="en">
 
 <head>
-    <meta charset="utf-8">
-    
-    <!--====== Title ======-->
-    <title>Smash - Bootstrap Business Template</title>
-    
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="description" content="Job Vacancies Website">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <title>Mimpy.ID</title>
 
-    <!--====== Favicon Icon ======-->
-    <link rel="shortcut icon" href="/images/landing-page/favicon.png" type="image/png">
-        
-    <!--====== Magnific Popup CSS ======-->
-    <link rel="stylesheet" href="/css/magnific-popup.css">
-        
-    <!--====== Slick CSS ======-->
-    <link rel="stylesheet" href="/css/slick.css">
-        
-    <!--====== Line Icons CSS ======-->
-    <link rel="stylesheet" href="/css/LineIcons.css">
-        
-    <!--====== Bootstrap CSS ======-->
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
-    
-    <!--====== Default CSS ======-->
-    <link rel="stylesheet" href="/css/default.css">
-    
-    <!--====== Style CSS ======-->
-    <link rel="stylesheet" href="/css/landing-page.css">
-    
+  <link rel="shortcut icon" href="/images/landing-page/favicon.png" type="image/png">
+
+  <link rel="stylesheet" href="/css/magnific-popup.css">
+  <link rel="stylesheet" href="/css/slick.css">
+  <link rel="stylesheet" href="/css/LineIcons.css">
+  <link rel="stylesheet" href="/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/default.css">
+  <link rel="stylesheet" href="/css/landing-page.css">
 </head>
 
 <body>
-    <!--[if IE]>
-    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-  <![endif]-->
-   
-    <!--====== PRELOADER PART START ======-->
 
-    <div class="preloader">
-        <div class="loader">
-            <div class="ytp-spinner">
-                <div class="ytp-spinner-container">
-                    <div class="ytp-spinner-rotator">
-                        <div class="ytp-spinner-left">
-                            <div class="ytp-spinner-circle"></div>
-                        </div>
-                        <div class="ytp-spinner-right">
-                            <div class="ytp-spinner-circle"></div>
-                        </div>
+  {{-- preload --}}
+  <div class="preloader">
+    <div class="loader">
+      <div class="ytp-spinner">
+        <div class="ytp-spinner-container">
+          <div class="ytp-spinner-rotator">
+            <div class="ytp-spinner-left">
+              <div class="ytp-spinner-circle"></div>
+            </div>
+            <div class="ytp-spinner-right">
+              <div class="ytp-spinner-circle"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  {{-- navbar --}}
+  <section class="navbar-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <nav class="navbar navbar-expand-lg">
+
+                    <ul class="navbar-nav navbar-brand">
+                        <li class="nav-item active"><a class="page-scroll" href="#home">Mimpy.ID</a></li>
+                    </ul>
+                    
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTwo" aria-controls="navbarTwo" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="toggler-icon"></span>
+                        <span class="toggler-icon"></span>
+                        <span class="toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse sub-menu-bar" id="navbarTwo">
+                        <ul class="navbar-nav m-auto">
+                            <li class="nav-item active"><a class="page-scroll" href="#home">home</a></li>
+                            <li class="nav-item"><a class="page-scroll" href="#services">Services</a></li>
+                            <li class="nav-item"><a class="page-scroll" href="#job">Job</a></li>
+                            <li class="nav-item"><a class="page-scroll" href="#about">About</a></li>
+                            <li class="nav-item"><a class="page-scroll" href="#contact">Contact</a></li>
+                        </ul>
                     </div>
-                </div>
+                    
+                    <div class="navbar-btn d-none d-sm-inline-block">
+                        <ul>
+                            <li><a class="solid" href="/register">Register</a></li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
         </div>
     </div>
-
-    <!--====== PRELOADER PART ENDS ======-->
-    
-    <!--====== NAVBAR TWO PART START ======-->
-
-    <section class="navbar-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <nav class="navbar navbar-expand-lg">
-                       
-                        <a href="#">
-                          <span  class="navbar-brand">
-                            Mimpy.ID
-                          </span>
-                        </a>
-                        
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTwo" aria-controls="navbarTwo" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="toggler-icon"></span>
-                            <span class="toggler-icon"></span>
-                            <span class="toggler-icon"></span>
-                        </button>
-
-                        <div class="collapse navbar-collapse sub-menu-bar" id="navbarTwo">
-                            <ul class="navbar-nav m-auto">
-                                <li class="nav-item active"><a class="page-scroll" href="#home">home</a></li>
-                                <li class="nav-item"><a class="page-scroll" href="#services">Services</a></li>
-                                <li class="nav-item"><a class="page-scroll" href="#job">Job</a></li>
-                                <li class="nav-item"><a class="page-scroll" href="#about">About</a></li>
-                                <li class="nav-item"><a class="page-scroll" href="#contact">Contact</a></li>
-                            </ul>
-                        </div>
-                        
-                        <div class="navbar-btn d-none d-sm-inline-block">
-                            <ul>
-                                <li><a class="solid" href="#">Download</a></li>
-                            </ul>
-                        </div>
-                    </nav> <!-- navbar -->
-                </div>
-            </div> <!-- row -->
-        </div> <!-- container -->
-    </section>
+  </section>
 
     <!--====== NAVBAR TWO PART ENDS ======-->
     
